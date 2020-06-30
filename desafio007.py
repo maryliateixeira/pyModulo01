be5ff052fef4885f975d7ecb63a7021830784d35
maryliateixeira <alvesmarylia11@gmail.com>
2020-06-29 21:35:44 -0300

    altera nome das variaveis

--- desafio007.py
+++ desafio007.py
@@ -1,6 +1,6 @@
 # desenvolva um programa que leia as duas notas de um aluno, calcule e mostre a sua média
-n1 = float(input('Digite a primeira nota:'))
-n2 = float(input('Digite a segunda nota:' ))
-r = n1 + n2
-m = (n1+n2) /2
-print('A sua nota é {} e a sua Média é:{}' .format(r, m))
+nota1 = float(input('Digite a primeira nota:'))
+nota2 = float(input('Digite a segunda nota:' ))
+resposta = nota1 + nota2
+m = (nota1+nota2) /2
+print('A sua nota é {} e a sua Média é:{}' .format(resposta, m))
