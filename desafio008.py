9de5e6e32b9c5eb01fbba1441de9eb277705cb74
maryliateixeira <alvesmarylia11@gmail.com>
2020-06-30 22:07:57 -0300

    Adição e alteração de variáveis

--- desafio008.py
+++ desafio008.py
@@ -1,6 +1,11 @@
 #escreva um programa que leia o valor em metros o exiba convertido em centímetros e milímetros
-num = int(input('Digite o valor, em metros, a ser convertido:'))
-cent = num*100
-mili = num*1.000
-print('Temos então {} metros,\nconvertidos em {:.3f} centímetros \ne {:.3f} milímetros' .format(num, cent, mili))
+medida = float(input('Digite uma distancia, em metros, a ser convertido:'))
+cm = medida*100
+mm= medida*1000
+dm = medida*10
+dam = medida/10
+hm = medida/100
+km = medida/1000
+print('Temos então {} m,\nconvertidos em {:.3f} cm \n {:.3f} mm \n {:.3f} dm \n {:.3f} dam \n {:.3f} hm \n {:.3f} km ' .format(medida, cm, mm, dm, dam, hm, km))
+
 
