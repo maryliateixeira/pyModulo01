c878700746db4111b529900bf5ab1216e9f6e009
maryliateixeira <alvesmarylia11@gmail.com>
2020-06-30 22:50:09 -0300

    Formatação do código

--- desafio010.py
+++ desafio010.py
@@ -1,5 +1,6 @@
 #crie um programa que leia quanto dinheiro uma pessoa tem na carteira e mostre quantos dólares ela pode comprar. Considere 1 dolar= 5,23 reais
-real = float(input('Quantos reais voce tem na carteira?'))
-dolar = float(input('Qual a cotação do dolar?'))
-result = real/dolar
-print('Com a cotação em R${}, voce possui R${} reais, que é o equivalente a US${} dolares.' .format(dolar, real, result ))
+real = float(input('Quantos reais você tem na carteira? R$'))
+dolar = real / 5.50
+euro = real / 6.14
+print('Com R${:.2f} reais, você pode comprar US${:.2f} dolares e €{:.2f} euros'.format(real, dolar, euro ))
+
