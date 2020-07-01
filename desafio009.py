8aa44c963a4a2aed719d4d1af164f5b90aa36c51
maryliateixeira <alvesmarylia11@gmail.com>
2020-06-30 22:32:14 -0300

    Alteração de todo código

--- desafio009.py
+++ desafio009.py
@@ -1,16 +1,16 @@
 #faça um programa que leia um número inteiro qualquer e mostre sua tabuada
 num = int(input('Tabuada de Multiplicação, digite um número:'))
-n = num*0
-n1 = num*1
-n2 = num*2
-n3 = num*3
-n4 = num*4
-n5 = num*5
-n6 = num*6
-n7 = num*7
-n8 = num*8
-n9 = num*9
-n10 = num*10
+print('-' * 12)
+print('{} x {:2} = {}'.format(num, 1, num*1))
+print('{} x {:2} = {}'.format(num, 2, num*2))
+print('{} x {:2} = {}'.format(num, 3, num*3))
+print('{} x {:2} = {}'.format(num, 4, num*4))
+print('{} x {:2} = {}'.format(num, 5, num*5))
+print('{} x {:2} = {}'.format(num, 6, num*6))
+print('{} x {:2} = {}'.format(num, 7, num*7))
+print('{} x {:2} = {}'.format(num, 8, num*8))
+print('{} x {:2} = {}'.format(num, 9, num*9))
+print('{} x {:2} = {}'.format(num, 10, num*10))
+print('-' * 12)
 
-print('A tabuada de {} é: \n{}x0={} \n{}x1={} \n{}x2={} \n{}x3={} \n{}x4={} \n{}x=5{} \n{}x6={} \n{}x7={} \n{}x8={} \n{}x9={} \n{}x10={}' .format(num, num, n, num, n1, num, n2, num, n3, num, n4, num, n5, num, n6, num, n7, num, n8, num, n9, num, n10))
 
