c2bd7bb19cf170d5220ef1e90faf99ec8139cf4a
maryliateixeira <alvesmarylia11@gmail.com>
2020-06-30 23:48:50 -0300

    Formatação de variáveis

--- desafio012.py
+++ desafio012.py
@@ -1,6 +1,5 @@
 #faça um algoritmo que leia o preço e mostre seu novo preço, com 5% de desconto.
-valor = float(input('Digite o preço em real:'))
-a = 5 * valor
-b = a / 100
-print('O desconto será de {} reais.' .format(b))
+valor = float(input('Digite o preço do produto R$'))
+desconto = valor - (valor* 5/ 100)
+print('O produto que custava R${:.2f}, com desconto de 5% custará R${:.2f} reais.' .format(valor, desconto))
 
--- desafio013.py
+++ desafio013.py
@@ -1,5 +1,4 @@
 #faça um algoritmo que leia o salário de um funcionário e mostre seu novo salário, com 15% de aumento.
-a = float(input('Digite seu salário:'))
-b = 15 * a
-c = b / 100
-print('O aumento do salário será de {:.3f} porcento' .format(c))
+salario = float(input('Digite seu salário R$'))
+aumento = salario + (salario * 15 / 100)
+print('O salário é R${:.2f} e com o aumento de 15% será R${:.2f} reais' .format(salario, aumento))
