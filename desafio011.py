e115c2a75419deb691bee4d95f68b36a62cfd25e
maryliateixeira <alvesmarylia11@gmail.com>
2020-06-30 23:14:52 -0300

    alteração de variáveis

--- desafio011.py
+++ desafio011.py
@@ -1,7 +1,10 @@
 #faça um programa que leia a altura e a largura de uma parede em metros, calcule sua area e a quantidade de tinta
 # necessária para pintá-la, sabendo que cada litro de tinha, pinta uma area de 2 metros quadrados
+larg= float(input('Digite a largura da parede em metros:'))
 alt = float(input('Digite a altura da parede em metros:'))
-larg = float(input('Digite a largura da parede em metros:'))
-par = alt * larg
-tin = par / 2
-print('A Area da parede é de {} m²\nSendo necessário {} litros de tinta para pintá-la.'.format(par, tin))
+area = larg * alt
+tinta = area / 2
+print('Sua parede tem a dimensão de {}x{} e sua área é de {}m².'.format(larg, alt, area))
+print('Para pintá-la é necessário {}L de tinta .'.format( tinta))
+
+
