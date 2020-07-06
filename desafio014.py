278b446435d1c13f30c6cb9ea8e4dc730687774a
maryliateixeira <alvesmarylia11@gmail.com>
2020-07-06 19:24:51 -0300

    Adicionando exercicio

---
+++ desafio014.py
@@ -0,0 +1,4 @@
+#faca um algoritmo que converta a temperatura de celcius para fahrenheit
+c = float(input('Informe a temperatura em ºC: '))
+f = 9 * c / 5 + 32
+print('A temperatura de {}ºC corresponde a {}ºF!'.format(c, f))
